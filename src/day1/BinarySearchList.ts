8f15edd38b1962032bf7a1baeb9718f446b68e86
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2025-10-01 08:52:30 +0300

    implement binary_search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let low = 0;
+  let high = haystack.length - 1;
 
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const guess = haystack[mid];
+    if (guess === needle) {
+      return true;
+    }
+    if (guess < needle) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return false;
 }
