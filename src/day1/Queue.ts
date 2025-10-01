65b7375cfa0c0d7ecaf5c2ef0211bc00aa9b2c51
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2025-10-01 10:17:24 +0300

    implement Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,41 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    this.length++;
+    if (!this.tail && !this.head) {
+      this.head = this.tail = { value: item };
+    } else {
+      this.tail!.next = { value: item };
+      this.tail = this.tail!.next;
     }
+  }
 
-    enqueue(item: T): void {
+  deque(): T | undefined {
+    if (!this.head) return undefined;
+    this.length--;
 
-}
-    deque(): T | undefined {
+    const value = this.head.value;
+    this.head = this.head.next;
 
-}
-    peek(): T | undefined {
+    if (!this.head) this.tail = undefined;
 
-}
+    return value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
