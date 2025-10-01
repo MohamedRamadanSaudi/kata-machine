cb97be8a778b76fcfa4a9e12934485b77b70ded6
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2025-10-01 10:31:45 +0300

    implement Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,41 @@
+type Node<T> = {
+  value: T;
+  prev?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const newNode = { value: item } as Node<T>;
+    this.length++;
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    newNode.prev = this.head;
+    this.head = newNode;
+  }
+
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1);
+    if (!this.head) {
+      return undefined;
     }
 
-    push(item: T): void {
+    const value = this.head.value;
+    this.head = this.head.prev;
 
-}
-    pop(): T | undefined {
+    return value;
+  }
 
-}
-    peek(): T | undefined {
-
-}
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
